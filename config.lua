01d1468425d2fee4fb339e94c70d3b9d50cae86a
skellykat <47672755+skellykat@users.noreply.github.com>
2022-10-25 19:11:50 +0100

    New Stuff
    Added fuel setting option
    Added choice of mph or km/h

--- config.lua
+++ config.lua
@@ -1,3 +1,6 @@
 Config = {}
 
+Config.Setting = "MPH" -- This can either be KM/H or MPH depending on your preference.
+Config.FuelSetting = "LegacyFuel" -- This can be either LegacyFuel or ps-fuel depending on your preference.
+
 Config.Tesla = -429774847
