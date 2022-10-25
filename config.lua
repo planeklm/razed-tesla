da5c236803183692c61b7e949d5622227356fa95
KLM <91488137+planeklm@users.noreply.github.com>
2022-10-25 20:25:55 +0100

    Made it even.

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.Setting = "MPH" -- This can either be KM/H or MPH depending on your preference.
+Config.Setting = "KMH" -- This can either be KMH or MPH depending on your preference.
 Config.FuelSetting = "LegacyFuel" -- This can be either LegacyFuel or ps-fuel depending on your preference.
 
 Config.Tesla = -429774847
