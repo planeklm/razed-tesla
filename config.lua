66377cecebb0231c11922e721c00d6725879a5d4
FiveToes <88988197+ChickenMan23@users.noreply.github.com>
2022-10-25 23:17:11 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,6 @@
 Config = {}
 
+Config.Speed = "M" --  M = MPH      K = KMH (Do the same as below)
 Config.Setting = "KMH" -- This can either be KMH or MPH depending on your preference.
 Config.FuelSetting = "LegacyFuel" -- This can be either LegacyFuel or ps-fuel depending on your preference.
 
