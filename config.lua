adb6bb42e2703c23c8a103482850e0bbe8456581
KLM <91488137+planeklm@users.noreply.github.com>
2022-10-25 23:20:23 +0100

    More small changes.

--- config.lua
+++ config.lua
@@ -1,7 +1,8 @@
 Config = {}
 
-Config.Speed = "M" --  M = MPH      K = KMH (Do the same as below)
 Config.Setting = "KMH" -- This can either be KMH or MPH depending on your preference.
+Config.Speed = "K" --  K = KMH  M = MPH  (Do the same as below)
+
 Config.FuelSetting = "LegacyFuel" -- This can be either LegacyFuel or ps-fuel depending on your preference.
 
 Config.Tesla = -429774847 -- Put your car hashkey here
