f8219252b23d84252642fd936d7031207c78d5f8
FiveToes <88988197+ChickenMan23@users.noreply.github.com>
2022-10-25 23:17:37 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,4 +4,4 @@ Config.Speed = "M" --  M = MPH      K = KMH (Do the same as below)
 Config.Setting = "KMH" -- This can either be KMH or MPH depending on your preference.
 Config.FuelSetting = "LegacyFuel" -- This can be either LegacyFuel or ps-fuel depending on your preference.
 
-Config.Tesla = -429774847
+Config.Tesla = -429774847 -- Put your car hashkey here
