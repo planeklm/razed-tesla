b953b3c64f6d46bb79ae6fcef0fa5b2baaa42c26
KLM <91488137+planeklm@users.noreply.github.com>
2022-11-20 15:33:07 +0000

    Added support for wtfev.

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
 Config.Setting = "MPH" -- This can either be KMH or MPH depending on your preference.
-Config.FuelSetting = "LegacyFuel" -- This can be either 'LegacyFuel' or 'ps-fuel' or 'ox_fuel' depending on your preference.
+Config.FuelSetting = "LegacyFuel" -- This can be either 'LegacyFuel' or 'ps-fuel' or 'ox_fuel' or 'wtfev' depending on your preference.
 
 Config.Teslas = {
     -429774847, -- Can be hash number
